4815b1b564f159edba672580ed46ec899528327b
Waideloss <psykhokidzxc@gmail.com>
2024-09-13 00:56:38 +0700

    test code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Xd
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Sum(1, 2));
+    }
+}
