f787e8031e05aae08909fb605d6b8db65280ff50
Waideloss <psykhokidzxc@gmail.com>
2024-09-13 11:37:00 +0700

    create c# proj

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
-using System;
-
-namespace Xd
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine(Sum(1, 2));
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
